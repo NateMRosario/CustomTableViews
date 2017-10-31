8a779c79b094a121d1550fed228d6bb91b18c255
Nathan Rosario <nathanrosario@ac.c4q.nyc>
2017-10-31 14:22:03 -0400

    First question

---
+++ CustomTableViewCells/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  CustomTableViewCells
+//
+//  Created by C4Q on 10/31/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
--- CustomTableViewCells/ViewController.swift
+++ CustomTableViewCells/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var movies: [Movie] = []
 
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadData()
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        self.myTableView.rowHeight = UITableViewAutomaticDimension
+        self.myTableView.estimatedRowHeight = 200.0
+    }
+    func loadData() {
+        let allMovies = MovieData.movies
+        let sortedMovies = allMovies.sorted{$0.name < $1.name}
+        self.movies = sortedMovies
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    ///Two required Data Source methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Movie Cell", for: indexPath)
+        if let cell = cell as? MovieTableViewCell {
+            let rowToSetUp = indexPath.row
+            let movieToSetUp = movies[rowToSetUp]
+            cell.titleLabel.text = movieToSetUp.name
+            cell.descriptionLabel.text = movieToSetUp.description
+            cell.moviePosterImageView.image = UIImage(named: movieToSetUp.posterImageName)
+            return cell
+        } else {
+            return cell
+        }
     }
 
-
+    @IBAction func sortButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0 :
+            self.movies = movies.sorted{$0.genre < $1.genre}
+        case 1:
+            self.movies = movies.sorted{$0.name < $1.name}
+        default:
+            break
+        }
+        myTableView.reloadData()
+    }
 }
 
